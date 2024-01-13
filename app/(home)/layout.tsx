b52f913fe214ed63990f6e711ac24cda46ae6900
JarGad23 <j.gad@m8b.pl>
2024-01-13 18:11:28 +0100

    Fixes in home page and layout

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -4,7 +4,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <Navbar showLogo={true} />
-      <main className="pt-32">{children}</main>
+      <main className="pt-12">{children}</main>
     </div>
   );
 };
