27fcfbbee064a185d3daf08d876ad385232fb06e
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-01-15 21:04:32 +0100

    Feature 9: Create token trpc mutation (#11)
    * Package json changes
    
    * Imporovder create token form
    
    * css changes
    
    * Added Sonner
    
    * Create api for create token
    
    * Get user lib and changed name of form

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Provider } from "./_trpc/provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             >
               {children}
             </ThemeProvider>
+            <Toaster richColors position="top-center" />
           </Provider>
         </body>
       </html>
--- components/create-token-form.tsx
+++ components/create-token-form.tsx
@@ -4,12 +4,32 @@ import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { createCoinSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { CardWrapper } from "./card-wrapper";
+import { trpc } from "@/app/_trpc/client";
+import { toast } from "sonner";
 
-export const CreateCoinForm = () => {
+export const CreateTokenForm = () => {
+  const { mutate: createToken, isLoading } = trpc.createToken.useMutation({
+    onSuccess: () => {
+      form.reset();
+      toast.success("Token created successfully");
+    },
+    onError: ({ message }) => {
+      form.reset();
+      toast.error(message);
+    },
+  });
+
   const form = useForm<z.infer<typeof createCoinSchema>>({
     resolver: zodResolver(createCoinSchema),
     defaultValues: {
@@ -19,10 +39,14 @@ export const CreateCoinForm = () => {
     },
   });
 
+  const onSubmit = (values: z.infer<typeof createCoinSchema>) => {
+    createToken({ ...values });
+  };
+
   return (
     <CardWrapper titleLabel="Create Coin">
       <Form {...form}>
-        <form className="space-y-6">
+        <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
           <div className="space-y-4">
             <FormField
               control={form.control}
@@ -31,8 +55,14 @@ export const CreateCoinForm = () => {
                 <FormItem>
                   <FormLabel>Name</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="Coin name" type="text" />
+                    <Input
+                      {...field}
+                      placeholder="Coin name"
+                      type="text"
+                      disabled={isLoading}
+                    />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -43,8 +73,14 @@ export const CreateCoinForm = () => {
                 <FormItem>
                   <FormLabel>Price</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="10.99" />
+                    <Input
+                      {...field}
+                      placeholder="10.99"
+                      type="number"
+                      disabled={isLoading}
+                    />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -55,13 +91,19 @@ export const CreateCoinForm = () => {
                 <FormItem>
                   <FormLabel>Quantity</FormLabel>
                   <FormControl>
-                    <Input {...field} placeholder="100" />
+                    <Input
+                      {...field}
+                      placeholder="100"
+                      type="number"
+                      disabled={isLoading}
+                    />
                   </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
           </div>
-          <Button type="submit" className="w-full">
+          <Button type="submit" className="w-full" disabled={isLoading}>
             Create
           </Button>
         </form>
