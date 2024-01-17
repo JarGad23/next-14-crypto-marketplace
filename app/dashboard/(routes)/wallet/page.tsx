2f75c52e4c463a3e6986b3021f1b6bded93a4ca8
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-01-17 17:45:03 +0100

    Feature 13: Create getting wallet api (#19)
    * Basic wallet page
    
    * Changed name to userWallet
    
    * Added api for getting user wallet

--- app/dashboard/(routes)/wallet/page.tsx
+++ app/dashboard/(routes)/wallet/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
+import { useAuth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 const WalletPage = () => {
-  return <div>Wallet Page</div>;
+  const { userId } = useAuth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const { data } = trpc.getUserWallet.useQuery({ walletUserId: userId });
+
+  return (
+    <div className="p-6 flex flex-col">
+      <div className="w-full max-w-4xl">
+        <h2 className="text-4xl font-bold">Your wallet 💸</h2>
+        <p className="text-muted-foreground">Here you can manage your tokens</p>
+      </div>
+      <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center gap-8">
+        {JSON.stringify(data)}
+      </div>
+    </div>
+  );
 };
 
 export default WalletPage;
