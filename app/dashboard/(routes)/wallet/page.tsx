dc56c91560fb6bd0c9b0cb5cef256636201ce1f4
JarGad23 <j.gad@m8b.pl>
2024-01-18 16:08:33 +0100

    Created a startup wallet page

--- app/dashboard/(routes)/wallet/page.tsx
+++ app/dashboard/(routes)/wallet/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 import { trpc } from "@/app/_trpc/client";
+import { TokenCard, TokenCardSkeleton } from "@/components/token-card";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useAuth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { AlertCircle, Wallet } from "lucide-react";
+import Link from "next/link";
+import { redirect, useRouter } from "next/navigation";
 
 const WalletPage = () => {
   const { userId } = useAuth();
+  const router = useRouter();
 
   if (!userId) {
     redirect("/");
   }
 
-  const { data } = trpc.getUserWallet.useQuery({ walletUserId: userId });
+  const {
+    data: tokens,
+    isError,
+    isLoading,
+  } = trpc.getUserWallet.useQuery({
+    walletUserId: userId,
+  });
+
+  const onClick = () => {
+    router.refresh();
+  };
 
   return (
     <div className="p-6 flex flex-col">
@@ -19,8 +35,59 @@ const WalletPage = () => {
         <h2 className="text-4xl font-bold">Your wallet 💸</h2>
         <p className="text-muted-foreground">Here you can manage your tokens</p>
       </div>
-      <div className="flex flex-col md:flex-row md:flex-wrap justify-center items-center gap-8">
-        {JSON.stringify(data)}
+      <div>
+        {isLoading ? (
+          <div className="pt-20 flex flex-col md:flex-row md:flex-wrap items-center gap-8">
+            {[...Array(6)].map((_) => (
+              <TokenCardSkeleton />
+            ))}
+          </div>
+        ) : isError ? (
+          <div className="pt-20 flex flex-col items-center justify-center gap-y-6">
+            <div className="flex items-center gap-x-2">
+              <h3 className="font-semibold text-3xl">Something went wrong</h3>
+              <AlertCircle className="h-8 w-8 ml-2 text-rose-500" />
+            </div>
+            <p className="text-xl text-muted-foreground">
+              Refresh or try again later
+            </p>
+            <Button variant="secondary" onClick={onClick}>
+              Refresh
+            </Button>
+          </div>
+        ) : (
+          <div className="pt-20 flex flex-col md:flex-row md:flex-wrap justify-center items-center gap-8">
+            {tokens.length === 0 ? (
+              <div className="pt-20 flex flex-col items-center justify-center gap-y-6">
+                <div className="flex items-center gap-x-2">
+                  <h3 className="font-semibold text-3xl">
+                    Your wallet is empty
+                  </h3>
+                  <Wallet className="h-10 w-10 ml-2" />
+                </div>
+                <span className="text-xl text-muted-foreground">
+                  You can get some tokens{" "}
+                  <Link
+                    href="/dashboard/browse"
+                    className="text-primary font-semibold text"
+                  >
+                    here
+                  </Link>{" "}
+                  or you can go and{" "}
+                  <Link
+                    href="/dashboard/create"
+                    className="text-primary font-semibold text"
+                  >
+                    create
+                  </Link>{" "}
+                  your own.
+                </span>
+              </div>
+            ) : (
+              tokens.map((token) => <TokenCard />)
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
