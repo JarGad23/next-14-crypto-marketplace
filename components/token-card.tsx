d8f9128aa432ab87c6b4a6c2384991268c07d743
JarGad23 <j.gad@m8b.pl>
2024-01-19 16:02:31 +0100

    Token card changes

--- components/token-card.tsx
+++ components/token-card.tsx
@@ -1,6 +1,3 @@
-"use client";
-
-import { trpc } from "@/app/_trpc/client";
 import {
   Card,
   CardContent,
@@ -12,33 +9,28 @@ import { Skeleton } from "./ui/skeleton";
 import Image from "next/image";
 import { formatPrice } from "@/lib/format-price";
 import { Button } from "./ui/button";
-import Link from "next/link";
+import { Token } from "@prisma/client";
+import { SellTokenForm } from "./sell-token-form";
 
 interface TokenCardProps {
-  tokenId: string;
+  token: Token;
   userQuantityOfToken: number;
 }
 
-export const TokenCard = ({ tokenId, userQuantityOfToken }: TokenCardProps) => {
-  const { data, isError } = trpc.getToken.useQuery({ tokenId });
-
-  if (!data || isError) {
-    return <TokenCardSkeleton />;
-  }
-
+export const TokenCard = ({ token, userQuantityOfToken }: TokenCardProps) => {
   return (
-    <Card className="w-full lg:w-[45%] 2xl:w-1/3">
+    <Card className="w-full lg:w-[45%] 2xl:w-[32%]">
       <CardHeader>
         <div className="flex items-center justify-between">
           <div>
-            <CardTitle className="text-3xl">{data.token.name}</CardTitle>
+            <CardTitle className="text-3xl">{token.name}</CardTitle>
             <CardDescription>
-              Created by: {data.token.creatorUserName}
+              Created by: {token.creatorUserName}
             </CardDescription>
           </div>
           <Image
             alt="token"
-            src={data.token?.imageUrl || ""}
+            src={token.imageUrl || ""}
             width="80"
             height="80"
             className="rounded-full aspect-square"
@@ -49,22 +41,25 @@ export const TokenCard = ({ tokenId, userQuantityOfToken }: TokenCardProps) => {
         <div className="flex items-center gap-x-2">
           <h3>
             Price per one token:{" "}
-            <span className="font-semibold">
-              {formatPrice(data.token.price)}
-            </span>
+            <span className="font-semibold">{formatPrice(token.price)}</span>
           </h3>
         </div>
         <div className="flex items-center gap-x-2">
           <h3>
             Quantity of tokens:{" "}
             <span className="font-semibold">
-              {`${userQuantityOfToken}/${data.token.quantity}`}
+              {`${userQuantityOfToken}/${token.quantity}`}
             </span>
           </h3>
         </div>
-        <Button asChild className="w-full">
-          <Link href="/dashboard/sell">Sell</Link>
-        </Button>
+        <SellTokenForm
+          tokenId={token.id}
+          price={token.price}
+          userQuantityOfToken={userQuantityOfToken}
+          name={token.name}
+        >
+          <Button className="w-full">Sell</Button>
+        </SellTokenForm>
       </CardContent>
     </Card>
   );
@@ -72,18 +67,20 @@ export const TokenCard = ({ tokenId, userQuantityOfToken }: TokenCardProps) => {
 
 export function TokenCardSkeleton() {
   return (
-    <div className="w-full lg:w-[45%] 2xl:w-1/4 h-60 rounded-md flex flex-col bg-gray-900 p-6">
-      <div className="w-full flex flex-col gap-y-2">
+    <div className="w-full lg:w-[45%] 2xl:w-[32%] h-60 rounded-md flex flex-col bg-gray-900 p-6">
+      <div className="w-full flex flex-col">
         <div className="flex justify-between items-center gap-x-2">
-          <Skeleton className="w-24 h-4" />
+          <div className="flex flex-col gap-y-2">
+            <Skeleton className="w-28 h-8" />
+            <Skeleton className="w-36 h-3" />
+          </div>
           <Skeleton className="w-20 h-20 rounded-full" />
         </div>
-        <Skeleton className="w-16 h-3" />
       </div>
-      <div className="w-full flex flex-col gap-y-4">
-        <Skeleton className="w-30 h-4" />
-        <Skeleton className="w-30 h-4" />
-        <Skeleton className="w-full h-6 rounded-md" />
+      <div className="w-full flex flex-col gap-y-4 mt-4">
+        <Skeleton className="w-32 h-4" />
+        <Skeleton className="w-32 h-4" />
+        <Skeleton className="w-full h-8 rounded-md" />
       </div>
     </div>
   );
