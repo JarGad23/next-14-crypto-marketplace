e3c909a0d85f397c948b8b71b1887a70175ade75
JarGad23 <j.gad@m8b.pl>
2024-01-20 12:06:54 +0100

    Implemented sell token form

--- components/sell-token-form.tsx
+++ components/sell-token-form.tsx
@@ -3,10 +3,22 @@
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { CardWrapper } from "./card-wrapper";
-import { Form } from "./ui/form";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Dialog, DialogClose, DialogContent, DialogTrigger } from "./ui/dialog";
 import { sellTokenSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { trpc } from "@/app/_trpc/client";
+import { toast } from "sonner";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { ElementRef, useRef } from "react";
 
 interface SellTokenFormProps {
   tokenId: string;
@@ -21,7 +33,20 @@ export const SellTokenForm = ({
   name,
   price,
   userQuantityOfToken,
+  tokenId,
 }: SellTokenFormProps) => {
+  const formCloseRef = useRef<ElementRef<"button">>(null);
+  const { mutate: sellToken, isLoading } = trpc.sellToken.useMutation({
+    onSuccess: () => {
+      form.reset();
+      toast.success("Token set for sale successfully");
+      window.location.reload();
+    },
+    onError: ({ message }: any) => {
+      toast.error(message);
+    },
+  });
+
   const form = useForm<z.infer<typeof sellTokenSchema>>({
     resolver: zodResolver(sellTokenSchema),
     defaultValues: {
@@ -32,15 +57,84 @@ export const SellTokenForm = ({
     },
   });
 
+  const onSubmit = (values: z.infer<typeof sellTokenSchema>) => {
+    sellToken({ tokenId, quantityForSale: values.quantityForSale });
+    formCloseRef?.current?.click();
+  };
+
   return (
     <Dialog>
-      <DialogTrigger>{children}</DialogTrigger>
+      <DialogTrigger className="w-full">{children}</DialogTrigger>
       <DialogContent>
         <CardWrapper titleLabel="Sell Token">
           <Form {...form}>
-            <form></form>
+            <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="space-y-4">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="text" disabled readOnly />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="price"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Price</FormLabel>
+                      <FormControl>
+                        <Input {...field} disabled readOnly />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="userQuantityOfToken"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Your amount of tokens</FormLabel>
+                      <FormControl>
+                        <Input {...field} disabled readOnly />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="quantityForSale"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Tokens you want to sale</FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder="10"
+                          {...field}
+                          disabled={isLoading}
+                          type="number"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <Button type="submit" className="w-full" disabled={isLoading}>
+                Sale
+              </Button>
+            </form>
           </Form>
         </CardWrapper>
+        <DialogClose ref={formCloseRef} />
       </DialogContent>
     </Dialog>
   );
