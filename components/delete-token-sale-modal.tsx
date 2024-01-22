df21305808a92d6d3aade43f78a8a8a0d449d29e
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-01-22 20:50:31 +0100

    Feature 21: Delete sale dialog (#34)
    * Added Delete token sale dialog
    
    * Delete uneccesery import

--- components/delete-token-sale-modal.tsx
+++ components/delete-token-sale-modal.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { trpc } from "@/app/_trpc/client";
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTrigger,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogAction,
+  AlertDialogCancel,
+} from "./ui/alert-dialog";
+import { toast } from "sonner";
+import { Button } from "./ui/button";
+
 interface DeleteTokenSaleModalProps {
   children: React.ReactNode;
   saleId: string;
@@ -7,5 +23,41 @@ export const DeleteTokenSaleModal = ({
   children,
   saleId,
 }: DeleteTokenSaleModalProps) => {
-  return <div className="w-full">{children}</div>;
+  const {
+    mutate: deleteSale,
+    isError,
+    isLoading,
+  } = trpc.deleteSellToken.useMutation({
+    onSuccess: (data) => {
+      toast.success("Token deleted successfully");
+      window.location.reload();
+    },
+    onError: ({ message }: any) => {
+      toast.error(message);
+    },
+  });
+
+  const onClick = () => {
+    deleteSale({ saleId });
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger className="w-full">{children}</AlertDialogTrigger>
+      <AlertDialogContent className="space-y-4">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Delete Sale</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action will delete sale, you can&apos;t retract this action
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <div className="flex items-center gap-x-2 justify-end">
+          <Button variant="destructive" asChild>
+            <AlertDialogAction onClick={onClick}>Delete</AlertDialogAction>
+          </Button>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+        </div>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
