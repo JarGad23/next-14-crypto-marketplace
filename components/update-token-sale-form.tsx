6a186fe13cf53a7e9a278d9fa7915a2164b5e581
JarGad23 <j.gad@m8b.pl>
2024-01-23 07:54:04 +0100

    Update token for sale form changes

--- components/update-token-sale-form.tsx
+++ components/update-token-sale-form.tsx
@@ -1,7 +1,35 @@
+"use client";
+import { trpc } from "@/app/_trpc/client";
+import { useAuth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 interface UpdateTokenSaleFormProps {
   children: React.ReactNode;
+  saleId: string;
+  quantityForSale: number;
+  sellingTokenId: string;
 }
 
-export const UpdateTokenSaleForm = ({ children }: UpdateTokenSaleFormProps) => {
+export const UpdateTokenSaleForm = ({
+  children,
+  saleId,
+  quantityForSale,
+  sellingTokenId,
+}: UpdateTokenSaleFormProps) => {
+  const { userId } = useAuth();
+
+  if (!userId) {
+    redirect("/");
+  }
+
+  const {
+    data: userTokenInWallet,
+    isError,
+    isLoading,
+  } = trpc.getTokenFromUserWallet.useQuery({
+    walletUserId: userId,
+    tokenId: sellingTokenId,
+  });
+
   return <div className="w-full">{children}</div>;
 };
