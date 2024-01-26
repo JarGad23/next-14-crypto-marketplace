2880861954ad36918369232d1da182e6bc7a462d
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-01-26 17:12:31 +0100

    Feature 27: Integrate stripe payments (#45)
    * Pacakge json changes
    
    * Middleware changes
    
    * Stripe lib create
    
    * Prisma schemas update
    
    * Added stripe webhook route
    
    * Buy token form change
    
    * Finished buy token route

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,140 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error ${error.message}`, { status: 500 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  const userId = session?.metadata?.userId;
+  const transactionId = session?.metadata?.transactionId;
+  const saleId = session?.metadata?.saleId;
+
+  if (event.type === "checkout.session.completed") {
+    if (!userId || !saleId || !transactionId) {
+      return new NextResponse(`Webhook Error: Missing metadata`, {
+        status: 400,
+      });
+    }
+
+    const existingTransaction = await db.transaction.findFirst({
+      where: { id: transactionId },
+    });
+
+    if (!existingTransaction) {
+      return new NextResponse("Failed to find transaction", { status: 404 });
+    }
+
+    const existingSale = await db.tokenForSale.findFirst({
+      where: { id: saleId },
+    });
+
+    if (!existingSale) {
+      return new NextResponse("Failed to find sale", { status: 404 });
+    }
+
+    const existingTokenInWallet = await db.userWallet.findUnique({
+      where: {
+        userId_tokenId: {
+          userId: existingTransaction.buyerUserId,
+          tokenId: existingTransaction.tokenId,
+        },
+      },
+    });
+
+    if (existingTokenInWallet) {
+      await db.userWallet.update({
+        where: {
+          id: existingTokenInWallet.id,
+        },
+        data: {
+          userQuantityOfToken: {
+            increment: existingTransaction.quantity,
+          },
+        },
+      });
+    } else {
+      await db.userWallet.create({
+        data: {
+          userId,
+          tokenId: existingTransaction.tokenId,
+          userQuantityOfToken: existingTransaction.quantity,
+        },
+      });
+    }
+
+    let sale;
+
+    if (existingTransaction.quantity === existingSale.quantityForSale) {
+      sale = await db.tokenForSale.delete({
+        where: {
+          id: existingSale.id,
+        },
+      });
+
+      await db.userWallet.delete({
+        where: {
+          userId_tokenId: {
+            tokenId: sale.tokenId,
+            userId: sale.sellerUserId,
+          },
+        },
+      });
+    } else {
+      sale = await db.tokenForSale.update({
+        where: {
+          id: existingSale.id,
+        },
+        data: {
+          quantityForSale: {
+            decrement: existingTransaction.quantity,
+          },
+          soldQuantity: {
+            increment: existingTransaction.quantity,
+          },
+        },
+      });
+    }
+
+    await db.transaction.update({
+      where: {
+        id: existingTransaction.id,
+      },
+      data: {
+        transactionState: "PAID",
+      },
+    });
+  } else {
+    await db.transaction.update({
+      where: {
+        id: transactionId,
+      },
+      data: {
+        transactionState: "DENIED",
+      },
+    });
+
+    return new NextResponse(
+      `Webhook Error: Unhandled event type ${event.type}`,
+      { status: 200 }
+    );
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
--- server/router/tokens.ts
+++ server/router/tokens.ts
@@ -4,6 +4,8 @@ import { createTokenSchema } from "@/schemas";
 import { getUserById } from "@/lib/get-user";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/lib/db";
+import Stripe from "stripe";
+import { stripe } from "@/lib/stripe";
 
 export const tokensRouter = router({
   createToken: privateProcedure
@@ -107,7 +109,102 @@ export const tokensRouter = router({
       })
     )
     .mutation(async ({ ctx, input }) => {
-      let url = "";
-      return url;
+      const { userId } = ctx;
+      const { saleId, quantityForBuy } = input;
+
+      const user = await getUserById(userId);
+
+      if (!userId) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+
+      if (!saleId || !quantityForBuy) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Missing required saleId or quantity for buy",
+        });
+      }
+
+      const existingSale = await db.tokenForSale.findFirst({
+        where: {
+          id: saleId,
+        },
+      });
+
+      if (!existingSale) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Sale not found" });
+      }
+
+      if (quantityForBuy > existingSale.quantityForSale) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "You cannot buy more than quantity for sale",
+        });
+      }
+
+      let transaction = await db.transaction.create({
+        data: {
+          tokenId: existingSale.tokenId,
+          quantity: quantityForBuy,
+          pricePerToken: existingSale.pricePerToken,
+          sellerUserId: existingSale.sellerUserId,
+          buyerUserId: userId,
+          transactionState: "PENDING",
+        },
+        include: {
+          token: true,
+        },
+      });
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [
+        {
+          quantity: quantityForBuy,
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: transaction.token.name,
+              images: [transaction.token.imageUrl!],
+            },
+            unit_amount: Math.round(transaction.pricePerToken * 100),
+          },
+        },
+      ];
+
+      let stripeCustomer = await db.stripeCustomer.findUnique({
+        where: {
+          userId,
+        },
+        select: {
+          stripeCustomerId: true,
+        },
+      });
+
+      if (!stripeCustomer) {
+        const customer = await stripe.customers.create({
+          email: user.emailAddresses[0].emailAddress,
+        });
+
+        stripeCustomer = await db.stripeCustomer.create({
+          data: {
+            userId,
+            stripeCustomerId: customer.id,
+          },
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        customer: stripeCustomer.stripeCustomerId,
+        line_items,
+        mode: "payment",
+        success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard/wallet`,
+        cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/dashboard/browse?canceled=1`,
+        metadata: {
+          transactionId: transaction.id,
+          saleId: existingSale.id,
+          userId,
+        },
+      });
+
+      return session.url;
     }),
 });
