cdc1dadc38c79b805444459854cd754d0cd6689b
JarGad23 <j.gad@m8b.pl>
2024-01-27 12:53:24 +0100

    Added transactions trpc router

--- server/index.ts
+++ server/index.ts
@@ -2,11 +2,13 @@ import { router } from "./trpc";
 import { walletRouter } from "./router/wallet";
 import { salesRouter } from "./router/sales";
 import { tokensRouter } from "./router/tokens";
+import { transactionsRouter } from "./router/transactions";
 
 export const appRouter = router({
   tokens: tokensRouter,
   wallet: walletRouter,
   sales: salesRouter,
+  transactions: transactionsRouter,
 });
 
 export type AppRouter = typeof appRouter;
