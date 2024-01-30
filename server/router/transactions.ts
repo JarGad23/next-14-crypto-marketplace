1c4816b5b517fcf89f601a41407d7561d27bfc22
JarGad23 <j.gad@m8b.pl>
2024-01-30 14:18:09 +0100

    Added user sales query

--- server/router/transactions.ts
+++ server/router/transactions.ts
@@ -2,6 +2,7 @@ import * as z from "zod";
 import { privateProcedure, router } from "../trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/lib/db";
+import { Sale } from "@/app/dashboard/_components/columns";
 
 export const transactionsRouter = router({
   getUserTransactions: privateProcedure.query(async ({ ctx }) => {
@@ -30,5 +31,40 @@ export const transactionsRouter = router({
     });
 
     return { userSales, userOrders };
+  }),
+  getSalesDataForTable: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    if (!userId) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+
+    const userSales = await db.transaction.findMany({
+      where: {
+        sellerUserId: userId,
+      },
+      select: {
+        token: {
+          select: {
+            name: true,
+          },
+        },
+        quantity: true,
+        pricePerToken: true,
+      },
+    });
+
+    if (!userSales) {
+      throw new TRPCError({ code: "NOT_FOUND" });
+    }
+
+    const resultData = userSales.map((sale): Sale => {
+      return {
+        name: sale.token.name,
+        quantity: sale.quantity,
+        total: sale.pricePerToken * sale.quantity,
+      };
+    });
+    return resultData;
   }),
 });
